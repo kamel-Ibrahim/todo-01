2d665b823cc2c7288668dba1ad95b820c74fb521
Kamel Ibrahim <Kamelk120212@gmail.com>
2025-10-19 19:47:21 +0300

    pastTask in app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import TaskList from './pages/TaskList';
 import Profile from './pages/Profile';
+import PastTasks from './pages/PastTasks';
 import './styles/App.css'; // Import global CSS
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/tasks" element={<TaskList />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/past-tasks" element={<PastTasks />} />
         </Routes>
       </div>
       <Footer />
