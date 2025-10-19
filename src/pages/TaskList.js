3bb3a12d51e6e3d19d565dc757c0393443b7d0b5
Kamel Ibrahim <Kamelk120212@gmail.com>
2025-10-19 19:44:46 +0300

    added import for taskform in tasklist

--- src/pages/TaskList.js
+++ src/pages/TaskList.js
@@ -1,19 +1,20 @@
 import React, { useContext } from 'react';
 import { TaskContext } from '../context/TaskContext';
+import TaskForm from '../components/TaskForm';
 import TaskCard from '../components/TaskCard';
 
 const TaskList = () => {
   const { tasks, removeTask } = useContext(TaskContext);
 
   return (
-    <div className="task-list">
-      {tasks.length === 0 ? (
-        <p className="no-tasks-message">No tasks available, please add some tasks!</p>
-      ) : (
-        tasks.map((task) => (
+    <div className="task-list-container">
+      <h1>Your Tasks</h1>
+      <TaskForm />
+      <div className="task-cards">
+        {tasks.map(task => (
           <TaskCard key={task.id} task={task} removeTask={removeTask} />
-        ))
-      )}
+        ))}
+      </div>
     </div>
   );
 };
