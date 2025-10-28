f63527a203d40ff13eae96ca39108133ede950ac
Zuhair <zuhairtinawi@gmail.com>
2025-10-28 10:44:26 +0200

    fixes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,9 @@
 import { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/AuthRegister.css";
-
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 export default function Register() {
   const { register } = useContext(AuthContext);
   const nav = useNavigate();
